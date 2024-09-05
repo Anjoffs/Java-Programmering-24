fd1b75d3eae64a45368caad89e280a225986ad25
Anjoffs <Andreas_965@hotmail.com>
2024-09-05 09:50:27 +0200

    hhh

---
+++ Coding_Playground/Java_Playground/Javaplayground.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Javaplayground{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        String = L;
+        L = sc.nextLine();
+
+        System.out.println("Input a letter");
+        
+
+
+    }
+}
